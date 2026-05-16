644e9b8dc6d124c92f7647427166cebb2aa6a471
卢骐征 <lqz.leo@gmail.com>
2026-05-16 16:33:52 +0800

    feat: add cell components (Hash, Message, Author, Date)

---
+++ git-client/src/components/commit/components/cells/HashCell.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+defineProps<{ hash: string }>()
+</script>
+
+<template>
+  <span class="hash-cell font-mono text-xs text-[var(--commit-text-secondary)] select-all">{{ hash.slice(0, 7) }}</span>
+</template>
--- git-client/src/components/commit/components/cells/MessageCell.vue
+++ git-client/src/components/commit/components/cells/MessageCell.vue
@@ -1,63 +1,20 @@
-<template>
-  <div class="message-cell" :style="{ width: width + 'px' }">
-    <span class="commit-hash font-mono">{{ commit.id.slice(0, 7) }}</span>
-    <span class="commit-message">
-      <template v-if="searchQuery">
-        <span
-          v-for="(part, i) in highlightedParts"
-          :key="i"
-          :class="{ 'search-highlight': part.isHighlight }"
-        >{{ part.text }}</span>
-      </template>
-      <template v-else>{{ firstLine }}</template>
-    </span>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { Commit } from '../../../../types/git'
-import { highlightText, getFirstLine } from '../../utils/commitHelpers'
+import { getFirstLine, highlightText } from '../../utils/commitHelpers'
 
 const props = defineProps<{
-  commit: Commit
-  width: number
-  searchQuery?: string
+  message: string
+  query?: string
 }>()
 
-const firstLine = computed(() => getFirstLine(props.commit.message))
-
-const highlightedParts = computed(() => {
-  if (!props.searchQuery) return [{ text: firstLine.value, isHighlight: false }]
-  return highlightText(firstLine.value, props.searchQuery)
-})
+const parts = computed(() => highlightText(getFirstLine(props.message), props.query ?? ''))
 </script>
 
-<style scoped>
-.message-cell {
-  padding: 0 8px;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  white-space: nowrap;
-}
-.commit-hash {
-  font-size: 12px;
-  color: #4fc3f7;
-  flex-shrink: 0;
-}
-.commit-message {
-  font-size: 12px;
-  color: var(--text-primary, #e0e0e0);
-  overflow: hidden;
-  text-overflow: ellipsis;
-  flex: 1;
-}
-.search-highlight {
-  background: rgba(255, 213, 79, 0.4);
-  color: #ffd54f;
-  border-radius: 2px;
-  padding: 0 1px;
-}
-</style>
+<template>
+  <span class="message-cell truncate text-sm leading-[40px]">
+    <template v-for="(part, i) in parts" :key="i">
+      <mark v-if="part.isHighlight" class="highlight bg-yellow-500/30 text-yellow-300 rounded px-0.5">{{ part.text }}</mark>
+      <span v-else>{{ part.text }}</span>
+    </template>
+  </span>
+</template>
